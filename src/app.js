cfc522f7bf6d14a8b27a09ecbd002e4e5389afc6
Fabio Monzani <renderbros@gmail.com>
2025-07-20 15:01:02 +0100

    Ordering and filtering articles

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 // Create a single supabase client for interacting with your database
 const supabase = createClient("https://krperkqbaqewikgzuoea.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtycGVya3FiYXFld2lrZ3p1b2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzMzU4NzcsImV4cCI6MTk5NTkxMTg3N30.ZiwrLZyY8lHlLspcVIagKrF5Bdci_R95lKpDDK56xHM");
 
-const { data, error } = await supabase.from("news_entries").select();
+const { data, error } = await supabase.from("news_entries").select().order("pub_date", { ascending: false }).limit(100);
 if (data) {
   const feedContainer = document.getElementById("feeds");
   let feed = "";
